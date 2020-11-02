61ba73814dc8c96e7de1cc60d253a2bcdeaa4484
jequezinho <jroverby92@gmail.com>
2020-11-01 22:26:49 -0600

    Added several functions to Pick and Player models, to calculate ROR

--- backend/app/models/pick.rb
+++ backend/app/models/pick.rb
@@ -12,4 +12,14 @@ class Pick < ApplicationRecord
     when 0.5
       "Push"
     end
+
+  def money
+    case @result
+    when 1
+      100.0
+    when 0
+      -110.0
+    when 0.5
+      0.0
+    end
 end
--- backend/app/models/player.rb
+++ backend/app/models/player.rb
@@ -1,2 +1,10 @@
 class Player < ApplicationRecord
+    def money_won
+        @picks.inject(0) { |sum, pick| sum + pick.money}
+    end
+
+    def ror
+        self.money_won / @picks.filter{ |pick| pick.result != 0.5}.length
+    end
+    
 end
