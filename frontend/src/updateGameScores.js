41b36e50386bd09eae44d6c0f574cfb71519e191
jequezinho <jroverby92@gmail.com>
2020-11-01 22:58:49 -0600

    Added file with functions to fetch finished games and update database accordingly

---
+++ frontend/src/updateGameScores.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const updateGames = () => {
+
+    const NFL_URL;
+    const [url, setUrl] = React.useState('');
+    const [count, setCount] = React.useState(0);
+    const someRef = React.useRef(0);
+
+    const updateGame = id => {
+        fetch(`${NFL_URL}/${id}`, {method: 'POST'})
+        .then(r=>r.json())
+        .then(data=>{})
+    }
+    
+    const lookForGames = () => {
+        fetch(NFL_URL)
+        .then(r=>r.json())
+        .then(games.forEach(game => {
+            if (game.finished) {
+                fetch(NFL)
+            }
+        }))
+    }
+    
+    React.useEffect(() => {
+        const timer = setInterval(() => tick(), 3600*100);
+        return () => clearInterval(timer);
+    });    
+    
+}
+
+
+ReactDOM.render(<updateGames/>, document.getElementById('root'));
