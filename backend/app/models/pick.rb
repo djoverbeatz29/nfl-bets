85f7ff77eb4e3ac1200eb24c175d4e674fa46cf4
jequezinho <jroverby92@gmail.com>
2020-11-01 22:12:10 -0600

    Added function to pick model, to render pick as win/loss/push

--- backend/app/models/pick.rb
+++ backend/app/models/pick.rb
@@ -2,4 +2,14 @@ class Pick < ApplicationRecord
   belongs_to :game
   belongs_to :player
   belongs_to :team
+
+  def pick_result
+    case @result
+    when 1
+      "Win"
+    when 0
+      "Loss"
+    when 0.5
+      "Push"
+    end
 end
