25764503f51d89ff05dc8bf98d45a943611fdd9d
jequezinho <jroverby92@gmail.com>
2020-11-09 15:49:43 -0600

    Significantly overhauled regex patterns & conditonal logic, to ensure multiword team names and half-point line portions were captured

--- frontend/src/getGamesInfo.js
+++ frontend/src/getGamesInfo.js
@@ -3,7 +3,7 @@ export default function getGamesInfo() {
     const info = [];
 
     fetch("https://www.vegasinsider.com/nfl/odds/las-vegas/")
-    .then(r=>r.json())
+    .then(r=>r.text())
     .then(data=>{
         // All the rows containing the individual game data
         const myRows =  [...document.querySelectorAll("table[class='frodds-data-tbl'] tr")];
@@ -12,29 +12,33 @@ export default function getGamesInfo() {
             // Declare object that will eventually hold the relevant pieces of data
             const rowData = {};
             // Variable storing inner text from cell with game time & team names
-            let rowText = row.querySelector("td[class*='viCellBg']").innerText;
+            let rowText = row.querySelector("td[class$=gameCell]").innerText;
             rowText = rowText.split('\n');
+            console.log(rowText);
 
             // Add year 2020 to the datetime string, then turn into Date object and add to rowData object
             let rowDate = rowText[0];
             rowDate = rowDate.split(' ');
-            rowDate[0] += "/2020";
+            rowDate[0] += `/${(new Date()).getFullYear()}`;
             rowDate = rowDate.join(' ');
             rowDate = new Date(rowDate);
             rowData['gameTime'] = rowDate;
+            console.log(rowDate);
 
             // Remove number from away team string, then add to rowData
             let awayTeam = rowText[1];
-            awayTeam = awayTeam.match(/[A-Za-z]+/)[0];
+            awayTeam = awayTeam.match(/[A-Za-z\. ]+/)[0];
             rowData['awayTeam'] = awayTeam;
+            console.log(awayTeam);
 
-            // Remove number from home team string, then add to rowData            
-            let homeTeam = rowText[1];
-            homeTeam = homeTeam.match(/[A-Za-z]+/)[0];
+            // Remove number from home team string, then add to rowData
+            let homeTeam = rowText[2];
+            homeTeam = homeTeam.match(/[A-Za-z\. ]+/)[0];
             rowData['homeTeam'] = homeTeam;
+            console.log(homeTeam);
 
             // Selects 2nd cell, which contains consensus line
-            const cells = row.querySelectorAll("td[class='viCellBg1 cellTextNorm cellBorderL1 center_text nowrap oddsCell']");
+            const cells = row.querySelectorAll("td[class$=oddsCell]");
             let spread = cells[1];
             let spreadText = spread.querySelector('a').innerText.trim().split('\n');
             let spreadAmount;
@@ -44,13 +48,18 @@ export default function getGamesInfo() {
             if (spreadText[0].match(/PK/) || spreadText[1].match(/PK/)) {
               spreadAmount = 0;
             }
-            else if (spreadText[0].match(/^-[0-9]+/)) {
-              spreadAmount = -parseInt(spreadText[0].match(/^-[0-9]+/));
+            else if (spreadText[0].match(/^-[0-9½\.]+/)) {
+              spreadAmount = spreadText[0].match(/^-[0-9½\.]+/)[0];
+              spreadAmount = spreadAmount.replace('½', '.5');
+              spreadAmount = -parseFloat(spreadAmount);
             }
-            else if (spreadText[1].match(/^-[0-9]+/)) {
-              spreadAmount = parseInt(spreadText[1].match(/^-[0-9]+/));
+            else if (spreadText[1].match(/^-[0-9½]+/)) {
+              spreadAmount = spreadText[1].match(/^-[0-9½\.]+/)[0];
+              spreadAmount = spreadAmount.replace('½', '.5');
+              spreadAmount = parseFloat(spreadAmount);
             }
             rowData['spread'] = spreadAmount;
+            console.log(spreadAmount);
 
             // After all data has been added to object, push object to info array
             info.push(rowData);
