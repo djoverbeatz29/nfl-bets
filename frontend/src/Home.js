9bdb7457751bc62027a8826422617e4f590a0471
jequezinho <jroverby92@gmail.com>
2020-11-10 16:10:37 -0600

    Deleted some unused code; added a snippet of JSX to calculate NFL week; a few other things

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,7 +3,6 @@ import Matchup from './Matchup';
 
 export default class Home extends React.Component {
     constructor(props) {
-        super(props)
         this.state = {
             games: this.props.games,
             player: this.props.player
@@ -13,7 +12,8 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="App">
-                <h1>NFL Schedule - Week 3</h1>
+                // Ridiculous looking code that actually yields the correct week, more or less!
+                <h1>NFL Schedule - Week {Math.ceil((new Date().getTime() - (new Date(2020, 8, 10)).getTime())/(86400*1000*7))}</h1>
                 {this.state.games.map((game, i) => <Matchup player={this.state.player} info={game} key={i}/> )}
             </div>
         )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { GoogleLogin } from 'react-google-login';
-
-// const respGoogle = resp => {
-//     console.log(resp);
-// }
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <GoogleLogin
-      clientId=""
-      buttonText="Login"
-      onSuccess={respGoogle}
-      onFailure={respGoogle}
-      cookiePolicy={'single_host_origin'}
-    /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
